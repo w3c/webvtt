25574d5eaf2479dd09fad11243f7149e5ebcb8c0
Philip Jägenstedt <philipj@opera.com>
2014-09-26 11:25:21 +0200

    Split static CG/WG spec generation into build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+# Generates a static WebVTT spec
+
+cd "$(dirname $0)"
+
+MODE="$1"
+if [ -z "$MODE" ]; then
+    MODE="-cg"
+fi
+
+DEST="$2"
+if [ -z "$DEST" ]; then
+    DEST="webvtt$MODE.html"
+fi
+
+TEMP="webvtt-tmp.html"
+
+sed "s/config-cg\.js/config$MODE.js/" webvtt.html > "$TEMP"
+phantomjs respec/tools/respec2html.js "$TEMP" "$DEST"
+rm -f "$TEMP"
